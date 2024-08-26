d4a6306a5072117adb36aaa398f335a3c1379b83
naddo210 <153388875+naddo210@users.noreply.github.com>
2024-08-26 20:56:35 +0530

    first-commit

---
+++ frontend/src/components/ViewMenu/Viewmenu.jsx
@@ -0,0 +1,186 @@
+import React from 'react'
+import "./viewmenu.css"
+const Viewmenu = () => {
+  return (
+    <div className='menu'>
+        <h1>Our menu</h1>
+        <hr />
+        <div className="menu-title">
+            <h1>Salad Menu</h1>
+            <div className="menu-info">
+                <p>Veg salad</p>
+                <p>Rs.10</p>
+            </div>
+            <div className="menu-info">
+                <p>Mix salad</p>
+                <p>Rs.5</p>
+            </div>
+            <div className="menu-info">
+                <p>Clover salad</p>
+                <p>Rs.15</p>
+            </div>
+            <div className="menu-info">
+                <p>Italian salad</p>
+                <p>Rs.60</p>
+            </div>
+
+        </div>
+        <hr />
+        <div className="menu-title">
+            <h1>Rolls Menu</h1>
+            <div className="menu-info">
+                <p>Mama meyo rolls</p>
+                <p>Rs.15</p>
+            </div>
+            <div className="menu-info">
+                <p>Veg rolls</p>
+                <p>Rs.25</p>
+            </div>
+            <div className="menu-info">
+                <p>Peri peri roll</p>
+                <p>Rs.25</p>
+            </div>
+            <div className="menu-info">
+                <p>Chicken rolls</p>
+                <p>Rs.45</p>
+            </div>
+
+        </div>
+        <hr />
+        <div className="menu-title">
+            <h1>Desserts Menu</h1>
+            <div className="menu-info">
+                <p>Fruit ice cream</p>
+                <p>Rs.22</p>
+            </div>
+            <div className="menu-info">
+                <p>Ripple ice cream</p>
+                <p>Rs.25</p>
+            </div>
+            <div className="menu-info">
+                <p>Vanila ice cream</p>
+                <p>Rs.10</p>
+            </div>
+            <div className="menu-info">
+                <p>Fruit ice cream</p>
+                <p>Rs.22</p>
+            </div>
+
+        </div>
+        <hr />
+        <div className="menu-title">
+            <h1>Sandwich Menu</h1>
+            <div className="menu-info">
+                <p>Sandwich</p>
+                <p>Rs.36</p>
+            </div>
+        
+            <div className="menu-info">
+                <p>Grill sandwich</p>
+                <p>Rs.10</p>
+            </div>
+            <div className="menu-info">
+                <p>Veg sandwich</p>
+                <p>Rs.13</p>
+            </div>
+            <div className="menu-info">
+                <p>Bread sandwich</p>
+                <p>Rs.16</p>
+            </div>
+
+        </div>
+        <hr />
+        <div className="menu-title">
+            <h1>Cake Menu</h1>
+            <div className="menu-info">
+                <p>Cup cake</p>
+                <p>Rs.15</p>
+            </div>
+            <div className="menu-info">
+                <p>Pastry</p>
+                <p>Rs.35</p>
+            </div>
+            <div className="menu-info">
+                <p>Vanila cake</p>
+                <p>Rs.19</p>
+            </div>
+           
+
+        </div>
+        <hr />
+        <div className="menu-title">
+            <h1>Pure-veg Menu</h1>
+            <div className="menu-info">
+                <p>Organic thaali</p>
+                <p>Rs.18</p>
+            </div>
+            <div className="menu-info">
+                <p>Mix veg thaali</p>
+                <p>Rs.17</p>
+            </div>
+            <div className="menu-info">
+                <p>Russian dish</p>
+                <p>Rs.16</p>
+            </div>
+           
+
+        </div>
+        <hr />
+        <div className="menu-title">
+            <h1>Pasta Menu</h1>
+            <div className="menu-info">
+                <p>Mayo pasta</p>
+                <p>Rs.12</p>
+            </div>
+            <div className="menu-info">
+                <p>Schezwan pasta</p>
+                <p>Rs.13</p>
+            </div>
+            <div className="menu-info">
+                <p>Cheezy pasta</p>
+                <p>Rs.14</p>
+            </div>
+            <div className="menu-info">
+                <p>Ring pasta</p>
+                <p>Rs.17</p>
+            </div>
+           
+
+        </div>
+        <hr />
+        <div className="menu-title">
+            <h1>Noodles Menu</h1>
+            <div className="menu-info">
+                <p>Khimchi noodles</p>
+                <p>Rs.12</p>
+            </div>
+            <div className="menu-info">
+                <p>Schezwan noodles</p>
+                <p>Rs.18</p>
+            </div>
+            <div className="menu-info">
+                <p>Chicken noodles</p>
+                <p>Rs.19</p>
+            </div>
+            
+           
+
+        </div>
+        <hr />
+        <div className="menu-title">
+            <h1>Halal Menu</h1>
+            <div className="menu-info">
+                <p>Biryani</p>
+                <p>Rs.80</p>
+            </div>
+           
+            
+           
+
+        </div>
+        
+    </div>
+  )
+}
+
+export default Viewmenu
